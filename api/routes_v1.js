2a4b0ad794b8ad7d71db7a716d457b30ec91d660
Arno Broeders <ahc.broeders@avans.nl>
2017-05-12 14:06:19 +0200

    Hello world naar Hello Avans

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -5,7 +5,7 @@ var express = require('express');
 var routes = express.Router();
 
 var mijnObject = { 
-	mijntekst: 'Hello World!',
+	mijntekst: 'Hello Avans!',
 	label: "Nog meer tekst",
 	mijnarray: [ "tekst", "nog meer tekst", 2 ],
 	mijnobject: {
